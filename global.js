67363aba9d6acd363f63b1d9e5f3330c0d12067c
Aliaksandr Maiseyeu <a.maiseyeu1.23@gmail.com>
2024-09-16 12:15:16 +0300

    Команды Global

---
+++ global.js
@@ -0,0 +1,22 @@
+//console.log(global);
+
+// setTimeout(()=>{
+//     console.log('Hello!');
+// }, 2000);
+
+
+// console.log(__dirname);
+
+// console.log(__filename);
+
+// console.log(process.env);
+
+// console.log(process.argv);
+
+// console.log(`hELLOW, ${process.argv[2]}`);
+
+const url = new URL('https://webDdev.com/path/name#test');
+console.log(url.hostname);
+console.log(url.href);
+console.log(url.pathname);
+console.log(url.hash);
